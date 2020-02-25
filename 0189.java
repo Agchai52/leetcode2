390ec572f9b1a2909b8deb23269b68612d9963bb
Shuang Zhang <37639111+Agchai52@users.noreply.github.com>
2020-02-25 18:36:38 -0500

    Create 0189.java

---
+++ 0189.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        int count = 0;
+        for (int start = 0; count < nums.length; start++) {
+            int cur = start;
+            int prev = nums[start];
+            do {
+                int next = (cur + k) % nums.length;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                cur = next;
+                count++;
+                    
+            } while (start != cur);
+        }
+        
+    }
+}
