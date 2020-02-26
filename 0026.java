6b10561967effc6c923ca08e475edddaba260fc6
Shuang Zhang <37639111+Agchai52@users.noreply.github.com>
2020-02-26 18:44:43 -0500

    Create 0026.java

---
+++ 0026.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[k] != nums[i]) {
+                k++;
+                nums[k] = nums[i];
+            }
+            
+        }
+        return k+1;
+    }
+}
