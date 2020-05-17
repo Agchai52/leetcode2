b6f39e678f63ae9822ecd3526681745cf11ba8ca
Shuang Zhang <37639111+Agchai52@users.noreply.github.com>
2020-05-16 23:16:27 -0400

    Create 0704.java

---
+++ 0704.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            int val = nums[m];
+            if (val == target) return m;
+            else if (val < target) l = m + 1;
+            else r = m - 1;
+        }
+        return -1;
+    }
+}
