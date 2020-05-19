fa13a6405861e8b04f96be4fbf4413026e6543a1
Shuang Zhang <37639111+Agchai52@users.noreply.github.com>
2020-05-18 20:03:34 -0400

    Create 0034.java

--- 0034.java
+++ 0034.java
@@ -3,28 +3,16 @@ class Solution {
         if (nums == null || nums.length == 0) return new int[]{-1, -1};
         int l = 0;
         int r = nums.length - 1;
-        int middle = 0;
-        while (l <= r) {
-            int m = l + (r - l) / 2;
-            if (nums[m] == target) {
-                l = m;
-                break;
-            }
-            else if (nums[m] < target) l = m + 1;
-            else r = m - 1;
-        }
-        if (l > r) return new int[]{-1, -1};
-        middle = l;
-        l = 0;
-        r = middle;
+        // find index i that nums[i] => target && nums[i - 1] < target
         while (l < r) {
             int m = l + (r - l) / 2;
             if (nums[m] < target) l = m + 1;
             else r = m;
         }
+        if (nums[l] != target) return new int[]{-1, -1};
         int res_l = l;
-        System.out.println(l);
-        l = middle;
+        
+        // find index j that nums[i] <= target && nums[i + 1] > target
         r = nums.length - 1;
         while (l + 1 < r) {
             int m = l + (r - l) / 2;
